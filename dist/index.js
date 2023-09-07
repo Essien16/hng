51cdde304fe0dbda5475b4c978c99d46f6c0ea61
Justice Essien <essienjustice@gmail.com>
2023-09-07 10:35:13 +0100

    updated build

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use("/api", require("../src/routes/hng"));
+const port = process.env.PORT || 3900;
+app.listen(port, console.log(`Port activated at port ${port}`));

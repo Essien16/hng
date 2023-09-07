0670c0dc06dc48ceac392eceff195096d5b87c8a
Justice Essien <essienjustice@gmail.com>
2023-09-07 09:41:02 +0100

    first commit

---
+++ src/controller/hngController.js
@@ -0,0 +1,38 @@
+const task1 = (req, res) => {
+    const slackName = req.query.slack_name;
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const currentDate = new Date();
+    const currentDay = daysOfWeek[currentDate.getUTCDay()];
+    const utcOffset = currentDate.getTimezoneOffset() / 60; // in hours
+    if (utcOffset < -2 || utcOffset > 2) {
+      res.status(400).json({ error: "Invalid UTC time" });
+      return;
+    }
+    const utcTime = new Date().toISOString();
+
+    const track = req.query.track;
+    const githubFileUrl = "https://github.com/essien16/hng/src/index.js";
+    const githubRepoUrl = "https://github.com/essien16/hng";
+
+    const response = {
+      slack_name: slackName,
+      current_day: currentDay,
+      utc_time: utcTime,
+      track: track,
+      github_file_url: githubFileUrl,
+      github_repo_url: githubRepoUrl,
+      status_code: 200,
+    };
+
+    res.json(response);
+};
+
+module.exports = {task1};
---
+++ src/routes/hng.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const {task1} = require("../controller/hngController")
+const router = express.Router();
+
+router.get("/", task1);
+
+module.exports = router;
